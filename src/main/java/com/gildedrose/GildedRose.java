4135f6d8f471297e57e4f42a783ab4b438e70426
SylvieDB <r0952804@student.thomasmore.be>
2023-12-13 21:08:16 +0100

    Refactored code (no test updates)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,84 @@
 package com.gildedrose;
 
-class GildedRose {
-    Item[] items;
+public class GildedRose {
+    private static final int MAX_QUALITY = 50;
+    private Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+    public void updateItem() {
+        for (Item item : items) {
+            updateQuality(item);
+            updateSellIn(item);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    public void updateSellIn(Item item) {
+        if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.sellIn--;
+        }
+    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+    public void updateQuality(Item item) {
+        if (qualityIsCorrect(item) == true) {
+            switch (item.name) {
+                case "Aged Brie":
+                    updateAgedBrie(item);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    updateBackstagePasses(item);
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    updateLegendary(item);
+                    break;
+                case "Conjured Mana Cake":
+                    updateConjured(item);
+                    break;
+                default:
+                    updateDefault(item);
             }
         }
     }
+
+    public boolean qualityIsCorrect(Item item){
+        if (item.quality>=0 && item.quality<=MAX_QUALITY){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void updateAgedBrie(Item item){
+            if(item.sellIn>=0){
+                item.quality ++;
+            }
+            else{
+                item.quality +=2;
+            }
+    }
+
+    public void updateBackstagePasses(Item item){
+            if(item.sellIn <=10){
+                item.quality +=2;
+            } else if (item.sellIn<=5){
+                item.quality +=3;
+            } else if (item.sellIn<=0){
+                item.quality=0;
+            }
+    }
+
+    public void updateLegendary(Item item){
+        //Nothing happens
+    }
+
+    public void updateConjured(Item item){
+            item.quality -=2;
+        }
+
+    public void updateDefault(Item item){
+            item.quality -=1;
+        }
 }
